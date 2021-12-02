fix normalization error and maintain one hot encoding.

baseline_debug.py
@@ -25,11 +25,18 @@ from utils.lr_scheduler import CosineAnnealingWarmUpRestarts
 import uuid
 from utils.training import train_one_epoch
 
+from utils.evaluation import all_together, draw_confusion_matrix
+
 device = torch.device('cuda')
 root: str = "../echocardiography/"
 
+num_classes = 2
+num_channel = 1
+
 transform = transforms.Compose([transforms.ToTensor(),
-                                transforms.Resize((256, 256))])
+                                transforms.Resize((256, 256),
+                                interpolation=transforms.InterpolationMode.NEAREST),
+                                ])
 
 train_a2c = os.path.join(root, 'train', 'A2C')
 train_a2c = ImageList.from_path(train_a2c, transform=transform, target_transform=transform)
@@ -50,10 +57,10 @@ val_datasets = ConcatDataset([val_a2c, val_a4c])
 
 # # Pretrained Model
 net = deeplabv3_resnet101(pretrained=True, progress=False)
-net.backbone.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
-net.classifier = DeepLabHead(2048, 1)
+net.backbone.conv1 = nn.Conv2d(num_channel, 64, kernel_size=7, stride=2, padding=3, bias=False)
+net.classifier = DeepLabHead(2048, num_classes)
 # net.aux_classifier = nn.Sequential()
-net.aux_classifier = FCNHead(1024, 1)
+net.aux_classifier = FCNHead(1024, num_classes)
 
 # # Non-pretrained Model
 # net = deeplabv3_resnet101(pretrained=False, num_classes=6)
@@ -68,7 +75,7 @@ if torch.cuda.device_count() > 1:
     net = torch.nn.DataParallel(net)
     net.to(device)
 
-print(torchinfo.summary(net, (1, 1, 256, 256)))
+print(torchinfo.summary(net, (1, num_channel, 256, 256)))
 
 # Lazy-eval iterable dataset: do not set sampler or shuffle options
 num_epoch = 1
@@ -118,15 +125,9 @@ for ep in range(num_epoch):
     if ep == num_epoch - 1:
         torch.save(state_dict(), os.path.join(checkpoint_dir, '{}.pt').format(ep))
 
-from utils.evaluation import all_together, draw_confusion_matrix
-
 label_names = [
     "Left Ventricle",
     "Background"
 ]
 
 _, _, _, _, cm = all_together(net, val_loader, device=device, verbose=True)
-draw_confusion_matrix(
-    cm[:5, :5], label_names, label_names,
-    figsize=(10, 8), title="Left Ventricle Division"
-)

models/loss.py
@@ -40,13 +40,13 @@ class BCEDiceIoUWithLogitsLoss2d(BCEDiceIoULoss2d):
     # can NOT be used in training, however it can be used in prediction.
     # see https://github.com/fastai/fastai/blob/master/fastai/metrics.py#L53
     def forward(self, logit, target):
-        bce_input = torch.sigmoid(logit)
+        bce_input = logit.softmax(dim=-3)
         bce = F.binary_cross_entropy(bce_input, target, weight=self.weight, reduction=self.reduction) * \
             self.bce_factor
         if self.training:
             probability = bce_input
         else:
-            probability = torch.round(bce_input)
+            probability = f.one_hot_nd(logit.argmax(dim=-3), logit.size(dim=-3), nd=2).to(logit.dtype)
         mul, add = probability * target, probability + target
         dice = f._dice_loss(mul, add, nd=2, reduction=self.reduction) * self.dice_factor  # noqa
         iou = f._iou_loss(mul, add, nd=2, reduction=self.reduction) * self.iou_factor  # noqa

utils/dataset.py
@@ -16,7 +16,7 @@ class ImageList(Dataset):
 
     def __init__(
             self, samples,
-            transform=None, target_transform=None,
+            transform=None, target_transform=None, num_classes=2,
             loader=pil_gray_loader, target_loader=numpy_loader
     ):
         self.samples = samples
@@ -24,6 +24,7 @@ class ImageList(Dataset):
         self.target_transform = target_transform
         self.loader = loader
         self.target_loader = target_loader
+        self.num_classes = num_classes
 
     @classmethod
     def from_path(
@@ -47,10 +48,21 @@ class ImageList(Dataset):
             sample = self.transform(sample)
         if self.target_transform is not None:
             target = self.target_transform(target)
+        target = self.get_one_hot_encoded_target(target)
         return sample, target
 
     def __len__(self):
         return len(self.samples)
+
+    def get_one_hot_encoded_target(self, target_img):
+        target = target_img.squeeze()
+        one_hot_target = np.zeros((self.num_classes, target.size(0), target.size(1)))
+
+        # the target not correctly normalize.
+        one_hot_target[0, :, :] = np.where(target == 0, 1, 0)
+        one_hot_target[1, :, :] = np.where(target > 0, 1, 0)
+
+        return one_hot_target
 
 
 def make_dataset(